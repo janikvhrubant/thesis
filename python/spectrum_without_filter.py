cf7bc86d976c8e3161e33931277e096acb26d7b4
Hrubant <janik.hrubant@sap.com>
2025-07-13 19:25:31 +0200

    wip

---
+++ python/spectrum_without_filter.py
@@ -0,0 +1,18 @@
+from spekpy import Spek
+import matplotlib.pyplot as plt
+
+# Create the spectrum with 80 kVp and 0.4 mm Sn filter
+spek = Spek(kvp=100, targ="W")
+
+# Get energies separately
+energies, intensities = spek.get_spectrum()
+print(energies)
+print(intensities)
+
+# Plot
+plt.plot(energies, intensities)
+plt.xlabel("Energy (keV)")
+plt.ylabel("Fluence")
+# plt.grid(True)
+plt.savefig("Figures/spectrum_without_filter.png", dpi=300, bbox_inches='tight')
+plt.show()
