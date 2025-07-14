3d248484019bff75841515b65d0183019ca043a3
Hrubant <janik.hrubant@sap.com>
2025-07-14 10:50:09 +0200

    compton

--- python/spekpy_1.py
+++ python/spekpy_1.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from spekpy import Spek # Import the SpekPy library for spectral calculations
 import matplotlib.pyplot as plt # Import pyplot for plotting
 from numpy import arange # Import stuff from numpy
@@ -15,6 +17,11 @@ spectrum.multi_filter(filters) # Apply the filtration to the spectrum
 spectrum = spectrum.get_spectrum() # Get the total fluence
 energy_keVs = spectrum[0]
 fluence = spectrum[1]
+probability = fluence / fluence.sum()
+
+u = np.random.rand()  # Sample a random number between 0 and 1
+sampled_energy = np.random.choice(energy_keVs, p=probability)  # Sample energy based on probability distribution
+print(f"Random number u: {u}, Sampled energy: {sampled_energy} keV")
 
 ## Plot the percentage against kV
 plt.plot(energy_keVs,fluence) 
