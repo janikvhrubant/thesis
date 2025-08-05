e53ac8f1773b30039c1ba19427772ac429b61feb
Hrubant <janik.hrubant@sap.com>
2025-08-05 21:05:23 +0200

    init

--- build_and_clean.sh
+++ build_and_clean.sh
@@ -4,8 +4,8 @@
 latexmk -pdf -interaction=nonstopmode main.tex
 
 # Nur wenn der Build erfolgreich war, dann löschen
-# if [ $? -eq 0 ]; then
-#     find . -type f \( -name '*.aux' -o -name '*.log' -o -name '*.toc' -o -name '*.bbl' -o -name '*.blg' -o -name '*.out' -o -name '*.fdb_latexmk' -o -name '*.fls' -o -name '*.synctex.gz' -o -name '*.run.xml' \) -delete
-# else
-#     echo "Build failed. Not cleaning up."
-# fi
+if [ $? -eq 0 ]; then
+    find . -type f \( -name '*.aux' -o -name '*.log' -o -name '*.toc' -o -name '*.bbl' -o -name '*.blg' -o -name '*.out' -o -name '*.fdb_latexmk' -o -name '*.fls' -o -name '*.synctex.gz' -o -name '*.run.xml' \) -delete
+else
+    echo "Build failed. Not cleaning up."
+fi
